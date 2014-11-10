33001f1ba42c628c9fbf475a23daeb712c010d2c
Sandeep S Kumar <sanygeek@gmail.com>
2014-11-11 00:27:39 +0530

    changed banner

--- client/js/main.js
+++ client/js/main.js
@@ -1,114 +1,27 @@
-// Banner animations
-
-function toggleMainContentScroll () {
-    var mainContent = $('.main-content');
-    if (mainContent[0].style.position === 'fixed') {
-        mainContent[0].style.position = 'absolute';
-    } else {
-        mainContent[0].style.position = 'fixed';
-    }
-}
+// banner animations
 
 $(function () {
     
-    var mainContent = $('.main-content');
-    var mainContentScrollable = false;
-    var currentScrollPosition = 0;
+    var duration = 0.5;
+    var wait = 4;
+    var tween;
     
-    var showCSS = {
-        top: '40%',
-        visibility: 'visible',
-        scale: '1'
-    };
-
-    var hideCSS = {
-        top: '120%',
-        visibility: 'hidden',
-        scale: '0.5'
-    };
-
-    var hiddenCSS = {
-        top: '-120%',
-        scale: '0.5'
-    };
-    
-    if(!mainContentScrollable) {
-        mainContent[0].style.position = 'fixed';
-    }
-    
-    var mouseDirection = '';
-    $(window).on('mousewheel', function(event) {
-        if (event.originalEvent.wheelDelta >= 0) {
-            mouseDirection = 'up';
-        }
-        else {
-            mouseDirection = 'down';
-        }
-    });
-    
-    $('.header__main-menu a').on('click', function () {
-        mainContent[0].style.position = 'absolute';
-        mainContentScrollable = true;
-    });
-    
-//    $(window).on('scroll', function () {
-//        if(window.scrollY < 10) {
-//            console.log('reset animation');
-//            TweenLite.to('.banner__questions.one, .banner__questions.two, .banner__questions.three', 0, hiddenCSS);
-//        }
-//    });
-    
-    activeQuestion = 0;
-    $(window).on("scrollstop", function(e) {
+    setTimeout(function animate () {
+        tween = new TimelineMax();
+        tween.repeat(-1);
         
-        if (mainContentScrollable) return;
+        tween.to('.banner__questions.one', duration, { x: '0'});
+        tween.to('.banner__questions.one', wait, { x: '0'});
+        tween.to('.banner__questions.one', duration, { x: '100%'});
         
-        switch (activeQuestion) {
-            case 0:
-                console.log('case: 0', activeQuestion)
-                if (mouseDirection === 'down') {
-                    TweenLite.to('.banner__questions.one', 0.5, showCSS);
-                    TweenLite.to('.banner__event-credits__block', 0.5, { opacity: 0 });
-                    activeQuestion++;
-                } else {
-                    TweenLite.to('.banner__questions.one', 0.5, hideCSS);
-                    mainContent[0].style.position = 'fixed';
-                    mainContentScrollable = false;
-                }        
-                break;
-            
-            case 1:
-                console.log('case: 1', activeQuestion)
-                if (mouseDirection === 'down') {
-                    TweenLite.to('.banner__questions.two', 0.5, showCSS);
-                    TweenLite.to('.banner__questions.one', 0.5, hiddenCSS);
-                    activeQuestion++;
-                } else {
-                    TweenLite.to('.banner__questions.two', 0.5, hideCSS);
-                    activeQuestion--;
-                }        
-                break;
-            
-            case 2:
-                console.log('case: 2', activeQuestion)
-                if (mouseDirection === 'down') {
-                    TweenLite.to('.banner__questions.three', 0.5, showCSS);
-                    TweenLite.to('.banner__questions.two', 0.5, hiddenCSS);
-                    activeQuestion++;
-                } else {
-//                    TweenLite.to('.banner__questions.three', 0.5, hideCSS);
-//                    activeQuestion--;
-                }        
-                break;
-            
-            case 3:
-                mainContent[0].style.position = 'absolute';
-                mainContentScrollable = true;
-                break;
-        }
+        tween.to('.banner__questions.two', duration, { x: '0'});
+        tween.to('.banner__questions.two', wait, { x: '0'});
+        tween.to('.banner__questions.two', duration, { x: '100%'});
         
+        tween.to('.banner__questions.three', duration, { x: '0'});
+        tween.to('.banner__questions.three', wait, { x: '0'});
+        tween.to('.banner__questions.three', duration, { x: '100%'});
         
-    });
-    
+    }, 1000);
     
 });
